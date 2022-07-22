8a4d2518030255a5d57156e70eeed66ad818b545
NaythanKik <dolapokiki3ird@gmail.com>
2022-07-22 18:28:27 +0100

    testing

--- facebook/index.js
+++ facebook/index.js
@@ -4,3 +4,18 @@ for (let x = 0; x < likes.length; x++) {
     likes[x].style.color = "blue";
   });
 }
+
+// let hamburger = document.querySelector(".menu");
+let menu = document.querySelector(".header-third");
+// hamburger.addEventListener("click", function (e) {
+//   if (menu.style.display == "none") {
+//     menu.style.display = "flex";
+//   } else {
+//     menu.style.display = "none";
+//   }
+// });
+
+let width = window.innerWidth;
+if (width <= 600) {
+  menu.style.display = "none";
+}
